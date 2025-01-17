81aef9a9990c127caf112144cc1756b9cd7820a1
Ethan BVSG <ebeauvaisguibert@gmail.com>
2025-01-17 14:13:07 +0100

    Flag

--- estrepublicain/public/js/index.js
+++ estrepublicain/public/js/index.js
@@ -1,16 +1,4 @@
 "use strict";
-fetch('api/videos')
-    .then(response => {
-      // Retourne une erreur s'il y en a une
-      if (!response.ok) {
-        throw new Error('Erreur d\'import des données');
-      }
-
-      // Conversion des données JSON en objet (avec une promesse)
-      response.json().then(data => {
-        console.log(data)
-      })
-    })
 
 const videoId = ["EsiugWDeZJw", "Ttb0F6Pamnk", "sq7tUSgvUPc", "C2RRR8uKrzk", "273ZNrAbhmI"]
 var filter = ""
@@ -20,7 +8,14 @@ const swiper = new Swiper("#mySwiper", {
   mousewheel: true,
   loop: true,
   pagination: false,
+  observer: true
 });
+
+var section = document.createElement("section")
+section.classList.add("swiper-slide")
+section.id = "intro"
+section.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus sapien, pharetra et pretium at, semper ac ex. Aenean et aliquam urna. Proin maximus, dolor non consequat commodo, turpis ligula aliquam est, quis molestie mauris lorem eget urna. Integer auctor libero nisl, sed commodo libero interdum eget. Nunc tristique congue sapien. Aliquam erat volutpat. Nam vehicula tincidunt bibendum. Morbi at massa sit amet felis faucibus eleifend. Quisque at condimentum nisi.\n"
+document.getElementsByClassName("swiper-wrapper")[0].appendChild(section)
 
 videoId.forEach((id, idx) => {
   var section = document.createElement("section")
@@ -30,6 +25,24 @@ videoId.forEach((id, idx) => {
   overlay.classList.add("h-100")
   overlay.classList.add("position-absolute")
   overlay.classList.add("z-1")
+  overlay.id = "videoOverlay" + idx.toString()
+
+  overlay.onclick = () => {
+    pauseResume(idx)
+  }
+
+  var pause = document.createElement("div")
+  pause.classList.add("pauseBtn")
+  pause.style.display = "none"
+  var pauseLogo = document.createElement("div")
+  pauseLogo.classList.add("pauseLogo")
+  pause.appendChild(pauseLogo)
+
+  var pauseLogo = document.createElement("div")
+  pauseLogo.classList.add("pauseLogo")
+  pause.appendChild(pauseLogo)
+
+  overlay.appendChild(pause)
   section.appendChild(overlay)
   var iframe = document.createElement("iframe")
 
@@ -64,12 +77,32 @@ setTimeout(() => {
 }, 1000);
 
 const initSlide = async function(slideNo){
-  document.getElementById("video" + (slideNo).toString()).src = "https://player.vimeo.com/video/1047771321?h=62e4427273&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&autoplay=1&loop=1&controls=0";
-  //document.getElementById("video" + (slideNo).toString()).src = "https://www.youtube.com/embed/" + videoId[slideNo] + "?autoplay=1&controls=0&showinfo=0&rel=0&t=0?playlist=" + videoId[slideNo];
+  if (slideNo === 0) {
+    document.getElementById("filters").style.display = "none"
+  }
+  else {
+    document.getElementById("intro").style.display = "none"
+    document.getElementById("video" + (slideNo-1).toString()).src = "https://player.vimeo.com/video/1047771321?h=62e4427273&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&autoplay=1&loop=1&controls=0";
+    document.getElementById("filters").style.display = "block"
+  }
 };
 
 const changeFilter = function(element) {
   [].forEach.call(document.getElementsByClassName("filter"), function (e) {e.classList.remove("selected")});
   element.classList.add("selected")
   filter = element.id
+}
+
+const pauseResume = function (id) {
+  var iframe = document.getElementById('video' + id.toString());
+  var player = new Vimeo.Player(iframe);
+  player.getPaused().then(function(paused) {
+    if (paused) {
+      player.play()
+      document.getElementById("pauseBtn").style.display = "none"
+    } else {
+      document.getElementById("pauseBtn").style.display = "flex"
+      player.pause()
+    }
+  });
 }
