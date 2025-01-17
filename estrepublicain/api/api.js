bf401feada87037f8ded6a651d729b7aead754a4
Noé Cabaud--Bloquel <noe.cabl@gmail.com>
2025-01-17 10:54:41 +0100

    feat: /videos and /videos/:category endpoints

--- estrepublicain/api/api.js
+++ estrepublicain/api/api.js
@@ -13,6 +13,36 @@ app.get('/topic', function ( req, res ) {
     res.json({'topic':topic});
 } );
 
+// Endpoint that sends all the videos
+app.get('/videos', function ( req, res ) {
+    let videos;
+
+    // Get videos.json
+    videos = require( '../public/data/videos.json' );
+    // Send it as a JSON object
+    res.json({videos});
+} );
+
+// Endpoint that sends the video correponding to a category
+app.get('/videos/:category', function (req, res) {
+    let videos;
+    let category = req.params.category;
+    let filteredVideos = {};
+
+    // Get videos.json
+    videos = require('../public/data/videos.json');
+
+    // Filter videos by category
+    for (let videoId in videos) {
+        if (videos[videoId].tags.includes(category)) {
+            filteredVideos[videoId] = videos[videoId];
+        }
+    }
+
+    // Send it as a JSON object
+    res.json({ videos: filteredVideos });
+});
+
 // app.get('/videos', async (req, res) => {
 //     // Récupération des artistes
 //     const response = await fetch(`/estrepublicain/public/data/videos.json`);
