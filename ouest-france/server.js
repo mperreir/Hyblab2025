24f6ab486eac70cd65d8cbb9d520349c04da1c84
Titouan GAUTIER <titoug@pop-os.localdomain>
2025-01-20 18:39:17 +0100

    alors peut etre

--- ouest-france/server.js
+++ ouest-france/server.js
@@ -1,4 +1,3 @@
-const { execSync } = require('child_process'); // Permet d'exécuter des commandes dans Node.js
 const express = require('express');
 const path = require('path');
 
@@ -11,12 +10,8 @@ app.use((req, res, next) => {
 
 console.log(__dirname)
 
-// Exécuter la commande de build
-//console.log('Building the React project...');
-//execSync('npm run build', { stdio: 'inherit' }); // Exécute "npm run build" et affiche les logs dans la console
-
 // Servir les fichiers statiques du build React sous le chemin /ouest-france/assets
-app.use('/ouest-france/assets', express.static(path.join(__dirname, 'dist/assets')));
+app.use('/ouest-france/dist/assets', express.static(path.join(__dirname, 'dist/assets')));
 app.use(express.static(path.join(__dirname, '../__common-logos__')));
 
 // Gérer les routes pour React (SPA)
