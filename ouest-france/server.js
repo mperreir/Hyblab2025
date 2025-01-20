f96c77a272681b20f14f737ea7650df34ef69733
Titouan GAUTIER <titoug@pop-os.localdomain>
2025-01-20 15:37:56 +0100

    je retest

--- ouest-france/server.js
+++ ouest-france/server.js
@@ -4,17 +4,14 @@ const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`Requête reçue pour: ${req.url}`);
-  next();
-});
+console.log(__dirname)
 
 // Exécuter la commande de build
 //console.log('Building the React project...');
 //execSync('npm run build', { stdio: 'inherit' }); // Exécute "npm run build" et affiche les logs dans la console
 
-// Servir les fichiers statiques du build React
-app.use(express.static(path.join(__dirname, 'dist')));
+// Servir les fichiers statiques du build React sous le chemin /ouest-france/assets
+app.use('/ouest-france/assets', express.static(path.join(__dirname, 'dist/assets')));
 app.use(express.static(path.join(__dirname, '../__common-logos__')));
 
 // Gérer les routes pour React (SPA)
