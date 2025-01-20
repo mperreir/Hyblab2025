bc0e3e561cf8138a0e112c6a7035636ef94f3a73
Titouan GAUTIER <titoug@pop-os.localdomain>
2025-01-20 18:47:56 +0100

    on y croit

--- ouest-france/server.js
+++ ouest-france/server.js
@@ -3,15 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`Requête reçue pour: ${req.url}`);
-  next();
-});
+// Servir les fichiers statiques générés par Vite
+app.use('/ouest-france', express.static(path.join(__dirname, 'dist')));
 
-console.log(__dirname)
-
-// Servir les fichiers statiques du build React sous le chemin /ouest-france/assets
-app.use('/ouest-france/dist/assets', express.static(path.join(__dirname, 'dist/assets')));
+// Servir les logos communs
 app.use(express.static(path.join(__dirname, '../__common-logos__')));
 
 // Gérer les routes pour React (SPA)
