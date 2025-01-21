251b1e1d2f98465ab77c68391255bd4167532fa1
Ruisi GAO <E24A563T@etu.univ-nantes.fr>
2025-01-22 00:57:43 +0100

    screen2

--- londeporteuse/public/js/screen2.js
+++ londeporteuse/public/js/screen2.js
@@ -1,24 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const options = document.querySelectorAll(".option");
-    const sizes = document.querySelectorAll(".size");
+const screen2ContainerId = "screen2-slide";
 
-    options.forEach(option => {
+// Stocker les s¨¦lections de l'utilisateur dans sessionStorage
+const storeUserSelections = () => {
+    const locationSelection = document.querySelector(`#${screen2ContainerId} .location-options .active`);
+    const sizeSelection = document.querySelector(`#${screen2ContainerId} .size-options .active`);
+
+    const userSelections = {
+        location: locationSelection ? locationSelection.id : null,
+        size: sizeSelection ? sizeSelection.id : null
+    };
+
+    sessionStorage.setItem("page2Selections", JSON.stringify(userSelections));
+};
+
+// G¨¦rer les clics sur les options
+const setupSelectionListeners = () => {
+    const locationOptions = document.querySelectorAll(`#${screen2ContainerId} .location-options .option`);
+    const sizeOptions = document.querySelectorAll(`#${screen2ContainerId} .size-options .size`);
+
+    locationOptions.forEach((option) => {
         option.addEventListener("click", () => {
-            options.forEach(opt => opt.classList.remove("selected"));
-            option.classList.add("selected");
+            locationOptions.forEach((opt) => opt.classList.remove("active"));
+            option.classList.add("active");
+            storeUserSelections();
         });
     });
 
-    sizes.forEach(size => {
+    sizeOptions.forEach((size) => {
         size.addEventListener("click", () => {
-            sizes.forEach(s => s.classList.remove("selected"));
-            size.classList.add("selected");
+            sizeOptions.forEach((opt) => opt.classList.remove("active"));
+            size.classList.add("active");
+            storeUserSelections();
         });
-
-
     });
+};
+
+const initializeScreen2 = () => {
+    const userSelections = JSON.parse(sessionStorage.getItem("page2Selections")) || {};
+
+    if (userSelections.location) {
+        const selectedLocation = document.querySelector(`#${screen2ContainerId} .location-options #${userSelections.location}`);
+        if (selectedLocation) selectedLocation.classList.add("active");
+    }
+
+    if (userSelections.size) {
+        const selectedSize = document.querySelector(`#${screen2ContainerId} .size-options #${userSelections.size}`);
+        if (selectedSize) selectedSize.classList.add("active");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeScreen2();
+    setupSelectionListeners();
 });
-
-
-
-
