125c6e584b8d22b4415b54501ff8e1711b8515b0
GitHub Action <action@github.com>
2025-01-22 23:10:31 +0000

    home creation

--- lequipe/src/src/app/app.routes.ts
+++ lequipe/src/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'roleplay', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'roleplay', component: RoleplayComponent }
 ];
--- lequipe/src/src/app/home/home.component.ts
+++ lequipe/src/src/app/home/home.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        CommonModule
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+    constructor(public router: Router) {}
 }
