f8b9f7e30e63b75517964a6eb36bd6a80f3b8d28
Titouan GAUTIER <titoug@pop-os.localdomain>
2025-01-23 10:56:51 +0100

    fix score

--- ouest-france/src/screens/Quiz.jsx
+++ ouest-france/src/screens/Quiz.jsx
@@ -1,25 +1,50 @@
 import React from "react";
 import { scrollToSection } from "../utils";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Quiz = ({ data }) => {
-
-  const [userAnswers, setUserAnswers] = useState(new Array(data.length).fill(null));
+  const [userAnswers, setUserAnswers] = useState(
+    new Array(data.length).fill(null)
+  );
   const [feedbacks, setFeedbacks] = useState(new Array(data.length).fill(""));
   const [score, setScore] = useState(0);
-  
+
+  useEffect(() => {
+    // Réinitialiser les réponses et le score lorsque les données du quiz changent
+    setUserAnswers(new Array(data.length).fill(null));
+    setFeedbacks(new Array(data.length).fill(""));
+    setScore(0);
+  }, [data]);
+
   const handleAnswerClick = (questionIndex, answer) => {
+    const previousAnswer = userAnswers[questionIndex];
     const newAnswers = [...userAnswers];
     newAnswers[questionIndex] = answer;
     setUserAnswers(newAnswers);
-    
+
     const question = data[questionIndex];
     const isCorrect = answer === question.correct_answer;
-    if (isCorrect) {
-      setScore(score + 1);
+    const wasCorrect = previousAnswer === question.correct_answer;
+
+    if (previousAnswer === null) {
+      console.log(previousAnswer);
+      console.log(newAnswers);
+      console.log(isCorrect);
+      // First time answering
+      if (isCorrect) {
+        setScore(score + 1);
+      }
+    } else {
+      // Changing the answer
+      if (isCorrect && !wasCorrect) {
+        setScore(score + 1);
+      } else if (!isCorrect && wasCorrect) {
+        setScore(score - 1);
+      }
     }
+
     const newFeedbacks = [...feedbacks];
-    newFeedbacks[questionIndex] = isCorrect 
+    newFeedbacks[questionIndex] = isCorrect
       ? "Correct ! " + (question.explanation || "")
       : "Incorrect. Essayez encore ! ";
     setFeedbacks(newFeedbacks);
@@ -54,13 +79,17 @@ const Quiz = ({ data }) => {
             )}
 
             {questionIndex > 0 && (
-              <button onClick={() => scrollToSection(`quiz${questionIndex - 1}`)}>
+              <button
+                onClick={() => scrollToSection(`quiz${questionIndex - 1}`)}
+              >
                 Previous Question
               </button>
             )}
 
             {questionIndex < data.length - 1 && (
-              <button onClick={() => scrollToSection(`quiz${questionIndex + 1}`)}>
+              <button
+                onClick={() => scrollToSection(`quiz${questionIndex + 1}`)}
+              >
                 Next Question
               </button>
             )}
@@ -71,16 +100,26 @@ const Quiz = ({ data }) => {
               </button>
             )}
 
-            <button onClick={() => scrollToSection(`article${questionIndex}`)}>Go To Article</button>
+            <button onClick={() => scrollToSection(`article${questionIndex}`)}>
+              Go To Article
+            </button>
           </section>
         ))}
-        <section 
-        className="h-screen w-screen flex-shrink-0 snap-start flex items-center justify-center flex-col bg-yellow-500"
-        id="quizResults"
+        <section
+          className="h-screen w-screen flex-shrink-0 snap-start flex items-center justify-center flex-col bg-yellow-500"
+          id="quizResults"
         >
           <h1 className="text-4xl font-bold">Résultats</h1>
-          <p className="text-2xl">Votre score est de {score}/{data.length}</p>
-          <button onClick={() => scrollToSection("quiz0")}>Retour au Quiz</button>
+          <p className="text-2xl">
+            Votre score est de {score}/{data.length}
+          </p>
+          <button
+            onClick={() => {
+              scrollToSection("quiz0");
+            }}
+          >
+            Retour au Quiz
+          </button>
         </section>
       </div>
     </div>
