c937460d95b68e5b9b6142e79cb2ec05b2784fea
Alan Petit <alan.petit@etu.univ-nantes.fr>
2025-01-23 16:15:01 +0100

    Page d'affichage des informations de contexte se développe à partir d'un message dans la discussion.

--- agence-api/public/js/main.js
+++ agence-api/public/js/main.js
@@ -1,58 +1,59 @@
-"use stric";
+"use strict";
 
 const initSlide2 = async function () {
 
-    const chatBox = document.getElementById('chatBox');
-    const messageList = document.getElementById('messageList');
-    const messageInput = document.getElementById('messageInput');
+  const chatBox = document.getElementById('chatBox');
+  const messageList = document.getElementById('messageList');
+  const messageInput = document.getElementById('messageInput');
 
-    scrollToBottom();
+  displayExplanation({titre:"test"});
+  scrollToBottom();
 
-    // Retrieve the intro's messages from our API
-    let response = await fetch('data/introStory.json');
-    const introStory = await response.json();
+  // Retrieve the intro's messages from our API
+  let response = await fetch('data/introStory.json');
+  const introStory = await response.json();
 
-    // Load the intro story
-    await loadIntroStory(introStory);
-    const userName = await getUserName();
-    
+  // Load the intro story
+  await loadIntroStory(introStory);
+  const userName = await getUserName();
+
 };
 
 function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function waitForLocalStorageKey(key, interval = 100) {
-    return new Promise((resolve) => {
-      const checkInterval = setInterval(() => {
-        const value = localStorage.getItem(key);
-        if (value !== null) {
-          clearInterval(checkInterval); // Stop checking once the value exists
-          resolve(value); // Resolve the promise with the value
-        }
-      }, interval);
-    });
-  }
-  
-async function loadIntroStory(introStory) {
-    document.getElementById('chat-input').style.visibility = 'hidden';
-
-    let i = 1;
-    for (const key in introStory) {
-        await wait(100); // Wait for 1 second
-        addMessage({ text: introStory[key], type: "received", timestamp: new Date().toISOString() });
-        scrollToBottom();
-        i++;
-    }
+  return new Promise((resolve) => {
+    const checkInterval = setInterval(() => {
+      const value = localStorage.getItem(key);
+      if (value !== null) {
+        clearInterval(checkInterval); // Stop checking once the value exists
+        resolve(value); // Resolve the promise with the value
+      }
+    }, interval);
+  });
 }
-  
+
+async function loadIntroStory(introStory) {
+  document.getElementById('chat-input').style.visibility = 'hidden';
+
+  let i = 1;
+  for (const key in introStory) {
+    await wait(100); // Wait for 1 second
+    addMessage({ text: introStory[key], type: "received", timestamp: new Date().toISOString() });
+    scrollToBottom();
+    i++;
+  }
+}
+
 async function getUserName() {
     document.getElementById('chat-input').style.visibility='visible';
-    document.getElementById('messageInput').focus();
+  document.getElementById('messageInput').focus();
 
-    userName = await waitForLocalStorageKey('userName');
-    
+  userName = await waitForLocalStorageKey('userName');
+
     document.getElementById('chat-input').style.visibility='hidden';
-    document.getElementById('messageInput').blur();
-    return userName;
+  document.getElementById('messageInput').blur();
+  return userName;
 }
--- agence-api/public/js/messages.js
+++ agence-api/public/js/messages.js
@@ -5,7 +5,7 @@
 //     const chatBox = document.getElementById('chatBox');
 //     const messageList = document.getElementById('messageList');
 
-document.onload = function() {
+document.onload = function () {
     const messageInput = document.getElementById('messageInput');
 
     messageInput.addEventListener('keydown', event => {
@@ -16,8 +16,6 @@ document.onload = function() {
 
     messageSendButton.addEventListener('click', sendMessage);
 };
-//     scrollToBottom();
-// };
 
 // Fonction pour envoyer un message
 function sendMessage() {
@@ -33,12 +31,30 @@ function sendMessage() {
     messageInput.value = '';
 };
 
+//première idée d'affichage de la page d'explication --> Contexte d'ouverture à adapter
+function displayExplanation(elemJSON) {
+    setTimeout(() => {
+        const reply = { text: elemJSON.titre, type: 'received', id: 'explanation' }; //id à adapter selon le parametrage du JSON
+        addMessage(reply);
+        //storeMessage(reply);
+        enableClickForExpansion(reply.text);
+
+        // Agrandissement automatique 1 seconde après apparition
+        setTimeout(() => {
+            const lastMessage = document.querySelector(`#${reply.id}`);
+            expandMessage(lastMessage);
+        }, 1000);
+    }, 1000);
+};
+
 // Fonction pour ajouter un message dans la liste
 function addMessage(message) {
     const messageElement = document.createElement('li');
     messageElement.classList.add('message', message.type);
     messageElement.textContent = message.text;
+    messageElement.id = message.id;
     messageList.appendChild(messageElement);
+    scrollToBottom();
 };
 
 // // Fonction pour effacer l'historique des messages (optionnelle)
@@ -52,5 +68,66 @@ function scrollToBottom() {
     setTimeout(() => {
         chatBox.scrollTop = chatBox.scrollHeight;
     }, 100);  // Délai pour s'assurer que le DOM est mis à jour
+};
+
+// Fonction pour permettre l'agrandissement manuel
+function enableClickForExpansion(text) {
+    const messages = document.querySelectorAll('.message.received');
+    messages.forEach(msg => {
+        if (msg.textContent === text) {
+            msg.addEventListener('click', () => {
+                expandMessage(msg);
+            });
+        }
+    });
+};
+
+// Fonction pour agrandir le message et afficher l'image
+function expandMessage(messageElement) {
+    const rect = messageElement.getBoundingClientRect();
+    expandingElement.querySelectorAll('*').forEach(element => element.style.display = 'none');
+    //Paramétrer ici l'ajout de contnenu selon les tags de la balise passée en paramètre
+
+    expandingElement.style.display = 'flex';
+    expandingElement.style.top = rect.top + 'px';
+    expandingElement.style.left = rect.left + 'px';
+    expandingElement.style.width = messageElement.offsetWidth + 'px';
+    expandingElement.style.height = messageElement.offsetHeight + 'px';
+
+    // Ajouter un écouteur d'événement pour afficher le contenu une fois la transition terminée
+    expandingElement.addEventListener('transitionend', function onTransitionEnd() {
+        expandingElement.querySelectorAll('*').forEach(element => element.style.display = 'flex');
+        expandingElement.removeEventListener('transitionend', onTransitionEnd);  // Nettoyer l'événement
+    });
+
+    setTimeout(() => {
+        expandingElement.style.top = '0';
+        expandingElement.style.left = '0';
+        expandingElement.style.width = '100vw';
+        expandingElement.style.height = '100vh';
+
+    }, 100);
+};
+
+// Fonction pour fermer l'overlay et réduire vers le message
+
+function closeOverlay() {
+    const lastMessage = document.querySelectorAll('.message.received');
+    // --> La séléection du message sur lequel fermer est à adapter selon tag des balises HTML ?
+    // Pour l'instant la fermeture est définie statiquement dans le HTML
+
+    expandingElement.querySelectorAll('*').forEach(element => element.style.display = 'none');
+    if (lastMessage.length > 0) {
+        const rect = lastMessage[lastMessage.length - 1].getBoundingClientRect();
+
+        expandingElement.style.top = rect.top + 'px';
+        expandingElement.style.left = rect.left + 'px';
+        expandingElement.style.width = lastMessage[lastMessage.length - 1].offsetWidth + 'px';
+        expandingElement.style.height = lastMessage[lastMessage.length - 1].offsetHeight + 'px';
+    }
+
+    setTimeout(() => {
+        expandingElement.style.display = 'none';
+    }, 500);
 };
 
