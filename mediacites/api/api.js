9a80d5f463e2b0a2f63ea13e1b69767cc33650ba
E239772X <pierre.pellerin@etu.univ-nantes.fr>
2025-01-23 10:40:32 +0100

    opérations api

--- mediacites/api/api.js
+++ mediacites/api/api.js
@@ -3,14 +3,44 @@
 const app = require( 'express' )();
 const path = require('path');
 
+const ip = `localhost:8080/mediacites`
+
 // Sample endpoint that sends the partner's name
-app.get('/topic', function ( req, res ) {
+app.get(`http://${ip}/api/categories`, function ( req, res ) {
     let topic;
 
     // Get partner's topic from folder name
     topic = path.basename(path.join(__dirname, '/..'))
     // Send it as a JSON object
     res.json({'topic':topic});
+} );
+
+app.get(`http://${ip}/api/articles/:category_name`, function ( req, res ) {
+    const category_name = req.params.articles.category_name;
+
+    // Send it as a JSON object
+    res.send({category_name});
+} );
+
+app.get(`http://${ip}/api/articles/:category_name/kpis`, function ( req, res ) {
+    const category_name = req.params.articles.category_name;
+
+    // Send it as a JSON object
+    res.send({});
+} );
+
+app.get(`http://${ip}/api/articles/:category_name/linked_categories`, function ( req, res ) {
+    const category_name = req.params.articles.category_name;
+
+    // Send it as a JSON object
+    res.send({});
+} );
+
+app.get(`http://${ip}/api/articles/:category_name/links`, function ( req, res ) {
+    const category_name = req.params.articles.category_name;
+
+    // Send it as a JSON object
+    res.send({});
 } );
 
 // Export our API
