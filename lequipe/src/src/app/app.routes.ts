26e92250dd05e5d6143eeb84faee292d3706886b
GitHub Action <action@github.com>
2025-01-23 13:34:58 +0000

    Merge branch 'main' of https://github.com/Valalol/Hyblab-angular

--- lequipe/src/src/app/app.routes.ts
+++ lequipe/src/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { RoleplayComponent } from './roleplay/roleplay.component';
 import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'roleplay', component: RoleplayComponent }
+    { path: 'roleplay', component: RoleplayComponent },
+    { path: 'test', component: TestComponent }
 ];
