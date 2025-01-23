6e3956e38269a601d7c1db691a7f95962ab9046b
Ghassen <gjrad4738@gmail.com>
2025-01-23 15:14:08 +0100

    creation de page etape et route

--- lavoixdunord/frontend/src/App.js
+++ lavoixdunord/frontend/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 
 import QuestionPage from "./QuestionPage/QuestionPage";
+import Etape from "./EtapePage/Etape";
 
 function App() {
-  return <QuestionPage />;
+  //return <Etape />;
+  //return <QuestionPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Etape />} />
+        <Route path="/etape1" element={<QuestionPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
