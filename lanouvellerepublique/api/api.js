ea4ef5cda53b53b2700929e45352e9ce5584aa7f
Julien CHATRY <chatryjulien@gmail.com>
2025-01-24 20:53:47 +0100

    Suppression cors api.js

--- lanouvellerepublique/api/api.js
+++ lanouvellerepublique/api/api.js
@@ -4,9 +4,11 @@ const app = require( 'express' )();
 const path = require('path');
 const cors = require('cors');
 
+/*
 app.use(cors({
     origin: ['http://localhost:5173', 'http://localhost:4173', 'http://hyblab.polytech.univ-nantes.fr/lanouvellerepublique/*'],
 }));
+*/
 
 // Sample endpoint that sends the partner's name
 app.get('/animals/*', function ( req, res ) {
