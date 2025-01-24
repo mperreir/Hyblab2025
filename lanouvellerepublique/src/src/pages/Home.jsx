7e60360450716bb39124f27325c6f5a4dea7810f
Christian Soh <christian.soh@etu.univ-nantes.fr>
2025-01-24 12:25:09 +0100

    Mise en forme du bouton start sur la page home

--- lanouvellerepublique/src/src/pages/Home.jsx
+++ lanouvellerepublique/src/src/pages/Home.jsx
@@ -7,20 +7,24 @@ function Home() {
 
   return (
     <>
-    <Header titre={"Dans leur peau"} textColor={"black"} isHome="true"/>
-    <div className='home_page'>
-      <div className='content'>
+      <Header titre={"Dans leur peau"} textColor={"black"} isHome="true" />
+      <div className='home_page'>
+        <div className='content'>
           <div className='titles'>
             <h1 className='main-title'>Découvrez des espèces menacées</h1>
             <p className='subtitle'>
               Découvrez des espèces animales en voie de disparitions ainsi que leurs informations
             </p>
           </div>
-        <button className="start-button" onClick={() => navigate("/map")}>
+          {/* <button className="start-button" onClick={() => navigate("/map")}>
           Start
-        </button>
+        </button> */}
+
+          <div className='start-button'>
+            <span className='start-button-text'>Explore</span>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
