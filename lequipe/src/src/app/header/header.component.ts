bf7c377b61803fd871ee646f423878ba84381afc
GitHub Action <action@github.com>
2025-01-24 13:17:55 +0000

    Merge branch 'main' of https://github.com/Valalol/Hyblab-angular

--- lequipe/src/src/app/header/header.component.ts
+++ lequipe/src/src/app/header/header.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     standalone: true,
     imports: [
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        CommonModule
     ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
