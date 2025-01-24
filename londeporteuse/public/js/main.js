f6bfd4e6f2a56168ee17b52a57149c2d210b281e
GraziellaLOKO <lokograziella@gmail.com>
2025-01-24 10:56:48 +0100

    - Ajout du style de la page d'accueil; - Ajout d'un autre header avec le logo de l'app pour les pages 2 et +; - Ajout d'un conteneur pour le contenu de l'app dans main.mustache;

--- londeporteuse/public/js/main.js
+++ londeporteuse/public/js/main.js
@@ -10,7 +10,7 @@ const layout = fs.readFileSync(path.join(__dirname,"..", "main.mustache"), "utf8
 const header = fs.readFileSync(path.join(__dirname,"..", "contentFixe", "header.mustache"), "utf8");
 const footer = fs.readFileSync(path.join(__dirname,"..", "contentFixe", "footer.mustache"), "utf8");
 
-// 3. Chargement du contenu de la page (homeContent)
+// 3. Chargement du contenu de la page (Accueil)
 const homeContent = fs.readFileSync(path.join(__dirname,"..", "0.mustache"), "utf8");
 
 // 4. Préparation des données pour Mustache
@@ -28,23 +28,24 @@ const contentFixe = {
 const output = Mustache.render(layout, dataHome, contentFixe);
 
 // 7. Écriture du résultat dans un fichier HTML (ou utilisation en serveur)
-fs.writeFileSync(path.join(__dirname,"..", "index_bis.html"), output, "utf8");
+fs.writeFileSync(path.join(__dirname,"..", "index.html"), output, "utf8");
 
 console.log("La page index_bis.html a été générée avec succès !");
 
 
+/*---------------------------------------------------------------------------------------------*/
 
-/*-------------------------------*/
+
 const headerLogo = fs.readFileSync(
-    path.join(__dirname, "..", "contentFixe", "headerLogo.mustache"), // nouveau header "light"
+    path.join(__dirname, "..", "contentFixe", "headerLogo.mustache"), // header avec le logo de l'appli
     "utf8"
 );
 
-const budgetContent = fs.readFileSync(path.join(__dirname, "..", "1.mustache"),"utf8");
+const choicesContent = fs.readFileSync(path.join(__dirname, "..", "1.mustache"),"utf8");
   
-// 5b. Préparation des données pour la page budget
-const dataBudget = {
-    dynamicContent: budgetContent
+// 5b. Préparation des données pour la page de choix
+const choice = {
+    dynamicContent: choicesContent
 };
 
 // Contenus fixes pour la deuxième page
@@ -54,9 +55,9 @@ const contentFixeBudget = {
   };
 
 // 6b. Rendu final
-const outputBudget = Mustache.render(layout, dataBudget, contentFixeBudget);
+const outputBudget = Mustache.render(layout, choice, contentFixeBudget);
 
 // 7b. Écriture du résultat dans un fichier HTML
-fs.writeFileSync(path.join(__dirname, "..", "budget.html"),outputBudget,"utf8");
+fs.writeFileSync(path.join(__dirname, "..", "choices.html"),outputBudget,"utf8");
 
-console.log("La page budget.html a été générée avec succès !");
+console.log("La page choices.html a été générée avec succès !");
