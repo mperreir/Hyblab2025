d5090a15ab2ea4e2e0f278a148cec379c4240dc7
Christian Soh <christian.soh@etu.univ-nantes.fr>
2025-01-24 12:02:01 +0100

    Ajout des textes sur la page Home

--- lanouvellerepublique/src/src/pages/Home.jsx
+++ lanouvellerepublique/src/src/pages/Home.jsx
@@ -10,6 +10,12 @@ function Home() {
     <Header titre={"Dans leur peau"} textColor={"black"} isHome="true"/>
     <div className='home_page'>
       <div className='content'>
+          <div className='titles'>
+            <h1 className='main-title'>Découvrez des espèces menacées</h1>
+            <p className='subtitle'>
+              Découvrez des espèces animales en voie de disparitions ainsi que leurs informations
+            </p>
+          </div>
         <button className="start-button" onClick={() => navigate("/map")}>
           Start
         </button>
