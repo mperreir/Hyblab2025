bb3e7d2608e24289f5e0e943c9d490e8fbc37c64
Frank Delvoye <frank.delvoye@etu.univ-nantes.fr>
2025-01-24 09:53:38 +0100

    Création dela page contexte (reste à ajouter les objectif écologique)

--- brief/src/components/ContexteComponent.jsx
+++ brief/src/components/ContexteComponent.jsx
@@ -2,17 +2,27 @@
 import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
 
+import isoVille from '../assets/isoVille.jpg';
+//import objectifs from '../../public/data/objectifs.JSON'
+
+
 const ContexteComponent = () => {
   return (
-    <Box sx={{ textAlign: 'center', padding: 3 }}>
-      <Typography variant="h4" sx={{ marginBottom: 2 }}>
-Ceci est le contexte
+    <Box sx={{ textAlign: 'center', padding: 0 }}>
+      <Typography variant="body1" sx={{ marginBottom: 2 }}>
+      Vous êtes nommé(e) Ministre de l’Énergie et du Climat de Gallia en 2025. Votre mission est de transformer ces mix pour atteindre la neutralité carbone en 2050 tout en assurant un approvisionnement en énergie stable, abordable et satisfaisant pour la population.
       </Typography>
-      <Typography variant="body1" sx={{ marginBottom: 4 }}>
-        Prêt à démarrer le jeu ? Rejoignez-nous pour une expérience immersive.
-      </Typography>
+      
+      <img src={isoVille} style={{ width: '80%', marginRight: '10%' }} sx={{ marginBottom: 2}}/>
+
+
+
       <Button variant="contained" color="primary" sx={{ padding: '10px 20px' }}>
+<<<<<<< HEAD
         Non
+=======
+        Suivant
+>>>>>>> 89e4b7b (Création dela page contexte (reste à ajouter les objectif écologique))
       </Button>
     </Box>
   );
