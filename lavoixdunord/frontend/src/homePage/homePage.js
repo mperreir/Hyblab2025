f22e1fb09594c93a0256d41c2468fbe5018ebb28
Ghassen <gjrad4738@gmail.com>
2025-01-24 13:50:38 +0100

    liaison entre page

--- lavoixdunord/frontend/src/homePage/homePage.js
+++ lavoixdunord/frontend/src/homePage/homePage.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './homePage.css';
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-
+    const navigate = useNavigate(); // Initialisation de navigate
+    
     return (
-        <div className="background">
-            <div className="container">
+        <div className="background_home">
+            <div className="container_home">
                 <p>Bonjour, le grand départ de la 112e édition du Tour de France est proche. Cette année, les cyclistes s’élancent de Lille, et cette année, vous êtes de la course ! </p>
                 <p><i><b>Bienvenue dans Le Défi des 3 maillots </b></i></p>
                 <p>Vous courez les trois premières étapes du Tour qui traversent le Nord et le Pas-de-Calais. 
                 Votre mission ?</p>
                 <p><i><b>Testez vos connaissances sur la région et décrocher les trois maillots emblématiques du tour.</b></i></p>
             </div>
-            <button className="button"><b>SUIVANT</b></button>
+            <button className="button_home" onClick={() => navigate('/regles')}><b>SUIVANT</b></button>
         </div>
     );
 };
