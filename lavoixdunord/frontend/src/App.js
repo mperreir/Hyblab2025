a7fd1f99d729fcf98aface8ae030559cc037a062
arwaouriemmi <arwaouriemmi@gmail.com>
2025-01-24 11:06:28 +0100

    adding logo page

--- lavoixdunord/frontend/src/App.js
+++ lavoixdunord/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Page0 from "./page0/page0";
 import QuestionPage from "./QuestionPage/QuestionPage";
 import DifficultyPage from "./DifficultyPage/DifficultyPage";
 import Etape from "./EtapePage/Etape";
@@ -10,11 +10,13 @@ function App() {
     <Router>
       <Routes>
         {/* Page par défaut : choix de difficulté */}
-        <Route path="/" element={<DifficultyPage />} />
+        <Route path="/difficulty" element={<DifficultyPage />} />
         {/* Page des étapes selon la difficulté */}
         <Route path="/etape/:difficulty" element={<Etape />} />
         {/* Page des questions selon la difficulté et l'étape */}
         <Route path="/etape/:difficulty/:id" element={<QuestionPage />} />
+        <Route path="/" element={<Page0 />} />
+      
       </Routes>
     </Router>
   );
