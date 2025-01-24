e23e616b6b95f23e770760c9e4a11383630e7e27
GraziellaLOKO <lokograziella@gmail.com>
2025-01-24 14:19:40 +0100

    - Génération des pages html 3,4 et 5 - Modification du css pour rendre a page 2 scrollable

--- londeporteuse/public/js/main.js
+++ londeporteuse/public/js/main.js
@@ -61,3 +61,73 @@ const outputBudget = Mustache.render(layout, choice, contentFixeBudget);
 fs.writeFileSync(path.join(__dirname, "..", "choices.html"),outputBudget,"utf8");
 
 console.log("La page choices.html a été générée avec succès !");
+
+/* -------------------------------------------------------------------------- */
+
+const resultsContent = fs.readFileSync(path.join(__dirname, "..", "2.mustache"),"utf8");
+
+// 5b. Préparation des données pour la page de choix
+const results = {
+    dynamicContent: resultsContent
+};
+
+// Contenus fixes pour la deuxième page
+const contentFixeResult = {
+    header: headerLogo, // On injecte le header "logo seulement"
+    footer: footer
+  };
+
+// 6b. Rendu final
+const outputResult = Mustache.render(layout, results, contentFixeResult );
+
+// 7b. Écriture du résultat dans un fichier HTML
+fs.writeFileSync(path.join(__dirname, "..", "results.html"), outputResult, "utf8");
+
+console.log("La page results.html a été générée avec succès !");
+
+/* -------------------------------------------------------------------------- */
+
+const ajustementsContent = fs.readFileSync(path.join(__dirname, "..", "3.mustache"),"utf8");
+
+// 5b. Préparation des données pour la page de choix
+const ajustements = {
+    dynamicContent: ajustementsContent
+};
+
+// Contenus fixes pour la deuxième page
+const contentFixeAjustement = {
+    header: headerLogo, // On injecte le header "logo seulement"
+    footer: footer
+  };
+
+// 6b. Rendu final
+const outputAjustement = Mustache.render(layout, ajustements, contentFixeAjustement );
+
+// 7b. Écriture du résultat dans un fichier HTML
+fs.writeFileSync(path.join(__dirname, "..", "ajustements.html"), outputAjustement, "utf8");
+
+console.log("La page ajustements.html a été générée avec succès !");
+
+
+/* -------------------------------------------------------------------------- */
+
+const afficheContent = fs.readFileSync(path.join(__dirname, "..", "4.mustache"),"utf8");
+
+// 5b. Préparation des données pour la page de choix
+const affiche = {
+    dynamicContent: afficheContent
+};
+
+// Contenus fixes pour la deuxième page
+const contentFixeAffiche = {
+    header: headerLogo, // On injecte le header "logo seulement"
+    footer: footer
+  };
+
+// 6b. Rendu final
+const outputAffiche = Mustache.render(layout, affiche, contentFixeAffiche );
+
+// 7b. Écriture du résultat dans un fichier HTML
+fs.writeFileSync(path.join(__dirname, "..", "affiche.html"), outputAffiche, "utf8");
+
+console.log("La page affiche.html a été générée avec succès !");
