11289c98c3722c188711eab5a6b70d8a492aa2f6
GitHub Action <action@github.com>
2025-01-25 22:14:46 +0000

    Merge branch 'main' of https://github.com/Valalol/Hyblab-angular

--- lequipe/src/src/app/slider/slider.component.ts
+++ lequipe/src/src/app/slider/slider.component.ts
@@ -1,28 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { ContentData } from '../models/content-data';
-import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'app-slider',
-  standalone: true,
-  imports: [],
-  templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+    selector: 'app-slider',
+    standalone: true,
+    imports: [],
+    templateUrl: './slider.component.html',
+    styleUrl: './slider.component.scss'
 })
-export class SliderComponent {
-  @Input() items?: ContentData[];
-  @Output() selected_output: EventEmitter<ContentData> = new EventEmitter();
-  selected?: ContentData;
+export class SliderComponent implements OnChanges {
+    @Input() items?: ContentData[];
+    @Output() selected_output: EventEmitter<ContentData> = new EventEmitter();
+    selected?: ContentData;
 
-  selectItem(item: ContentData, evt: EventTarget | null): void {
-    this.selected = item;
-    this.scrollToSelectedItem(evt as Element);
-  }
-  scrollToSelectedItem(evt: Element): void {
-    evt.scrollIntoView({ behavior: "auto", block: 'end', inline: 'center' });
-  }
+    ngOnChanges(): void {
+        if (this.items) this.selected = this.items[0];
+    }
 
-  start(item: ContentData) {
-    this.selected_output.emit(item);
-  }
+    scroll_detected(): void {
+        let element = document.elementFromPoint(window.innerWidth/2, window.innerHeight*3/10)?.parentElement
+        if (element && element.classList.contains('item') && !element.classList.contains('selected')) {
+            console.log(element);
+            element.click();
+        }
+    }
+
+    selectItem(item: ContentData, evt: EventTarget | null): void {
+        this.selected = item;
+        this.scrollToSelectedItem(evt as HTMLElement);
+    }
+
+    scrollToSelectedItem(evt: HTMLElement): void {
+        // tous les trucs commentés ont été testés et ne marchent pas
+
+        // evt.style.scrollSnapAlign = 'none';
+        // setTimeout(() => {
+        //     evt.style.scrollSnapAlign = 'center';
+        // }, 500);
+
+        // if (evt.parentElement) { evt.parentElement.style.scrollSnapType = 'none'; }
+        // setTimeout(() => {
+        //     if (evt.parentElement) { evt.parentElement.style.scrollSnapType = 'x mandatory'; }
+        // }, 500);
+
+        evt.scrollIntoView({ behavior: "smooth", block: 'end', inline: 'center' });
+    }
+
+    start(item: ContentData) {
+        this.selected_output.emit(item);
+    }
 }
