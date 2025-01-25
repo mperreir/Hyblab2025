13e2e1d45d61a180fbc96f0e4cd71b844ec048be
Théophile Tagne <tagnepierre9@gmail.com>
2025-01-25 19:01:08 +0100

    mise à jour de la page de bienvenue selon le nouveau design

--- lavoixdunord/frontend/src/page0/page0.js
+++ lavoixdunord/frontend/src/page0/page0.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./page0.css";
+import useBasename from "../hooks/useBasenameHook";
 
 const Page0 = () => {
+  const basename = useBasename();
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate("/home"); 
-    }, 1000);
+      navigate("/home");
+    }, 1000 * 5000);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, [navigate]);
 
-  return <div className="background_page0"></div>;
+  return <div className="background_page0">
+    <img src={`${basename}images/logo_jeu.png`} alt="Page 0 background" className="logo_page0" />
+  </div>;
 };
 
 export default Page0;
