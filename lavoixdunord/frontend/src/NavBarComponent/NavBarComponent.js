c197c71460e6b8ba5d9d8e919c426decb97b5f97
Théophile Tagne <tagnepierre9@gmail.com>
2025-01-26 02:28:38 +0100

    feat: add animated cyclist SVG to NavBarComponent for enhanced visual appeal

--- lavoixdunord/frontend/src/NavBarComponent/NavBarComponent.js
+++ lavoixdunord/frontend/src/NavBarComponent/NavBarComponent.js
@@ -109,7 +109,8 @@ const NavBarComponent = () => {
                                 ))}
                             </Breadcrumb>
                         ) : (
-                            <h6 className="mb-0">{getPageTitle()}</h6>
+                            <CyclistAnimation />
+                            // <h6 className="mb-0">{getPageTitle()}</h6>
                         )}
                     </div>
 
@@ -120,8 +121,122 @@ const NavBarComponent = () => {
                     animated
                     style={{ height: '5px' }}
                 /> */}
+
                 </Container>
             </Navbar>)
+    );
+};
+
+
+
+const CyclistAnimation = () => {
+    return (
+        <div className="cyclist-container" style={{ width: '40px', height: '40px' }}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 200 200"
+                className="cyclist-svg"
+            >
+                {/* Bicycle Wheels */}
+                <circle
+                    cx="50"
+                    cy="150"
+                    r="30"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                    fill="none"
+                >
+                    <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 50 150"
+                        to="360 50 150"
+                        dur="2s"
+                        repeatCount="indefinite"
+                    />
+                </circle>
+                <circle
+                    cx="150"
+                    cy="150"
+                    r="30"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                    fill="none"
+                >
+                    <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 150 150"
+                        to="360 150 150"
+                        dur="2s"
+                        repeatCount="indefinite"
+                    />
+                </circle>
+
+                {/* Cyclist Body */}
+                <circle cx="100" cy="80" r="10" fill="#6BADC6" />
+                <line
+                    x1="100"
+                    y1="90"
+                    x2="100"
+                    y2="120"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                />
+                <line
+                    x1="100"
+                    y1="100"
+                    x2="70"
+                    y2="130"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                />
+                <line
+                    x1="100"
+                    y1="100"
+                    x2="130"
+                    y2="130"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                />
+
+                {/* Pedaling Legs */}
+                <line
+                    x1="100"
+                    y1="120"
+                    x2="80"
+                    y2="150"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                >
+                    <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 100 120"
+                        to="360 100 120"
+                        dur="1s"
+                        repeatCount="indefinite"
+                    />
+                </line>
+                <line
+                    x1="100"
+                    y1="120"
+                    x2="120"
+                    y2="150"
+                    stroke="#6BADC6"
+                    strokeWidth="5"
+                >
+                    <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="360 100 120"
+                        to="0 100 120"
+                        dur="1s"
+                        repeatCount="indefinite"
+                    />
+                </line>
+            </svg>
+        </div>
     );
 };
 
