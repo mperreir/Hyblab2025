a1dbb53f9f1ffaf2804c715d7f0e091996a4098a
Théophile Tagne <tagnepierre9@gmail.com>
2025-01-27 21:19:44 +0100

    Refactor home page styles and adjust button spacing

--- lavoixdunord/frontend/src/homePage/homePage.js
+++ lavoixdunord/frontend/src/homePage/homePage.js
@@ -11,14 +11,14 @@ const HomePage = () => {
         <div className="background_home text-center">
             <div className="container_home p-4 mt-5">
                 <h1 className="text-white mb-4">BIENVENUE !</h1>
-                <div className="my-5">&nbsp;</div>
+                <div className="my-4">&nbsp;</div>
                 <p>Bonjour, le grand départ de la 112e édition du Tour de France est proche. Cette année, les cyclistes s’élancent de Lille, et cette année, vous êtes de la course ! </p>
                 <p><i><b>Bienvenue dans Le Défi des 3 maillots </b></i></p>
                 <p>Vous courez les trois premières étapes du Tour qui traversent le Nord et le Pas-de-Calais.
                     Votre mission ?</p>
                 <p><i><b>Testez vos connaissances sur la région et décrocher les trois maillots emblématiques du tour.</b></i></p>
             </div>
-            <button className="btn btn-outline-dark mt-4" onClick={() => navigate('/regles')}><b>SUIVANT</b></button>
+            <button className="btn btn-outline-dark mt-3" onClick={() => navigate('/regles')}><b>SUIVANT</b></button>
         </div>
     );
 };
