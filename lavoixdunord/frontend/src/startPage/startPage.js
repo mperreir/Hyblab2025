db22de6aab71cf90a31bba612d8b37ab67cfde14
youssef-ibnouali <youssef.ibnouali@etu.univ-nantes.fr>
2025-01-27 12:00:29 +0100

    +upsate text startPage

--- lavoixdunord/frontend/src/startPage/startPage.js
+++ lavoixdunord/frontend/src/startPage/startPage.js
@@ -31,7 +31,7 @@ const StartPage = () => {
 
                 <div className="mx-3 p-3">
                     <p><i><b>Bienvenue à Lille, point de départ du Tour de France et de votre aventure.</b></i></p>
-                    <p>Votre première question vous attend, répondez avec précision, avancez sans perdre de temps et préparez-vous à découvrir les trésors cachés du Nord.</p>
+                    <p>  Votre première question vous attend, répondez avec précision, avancez sans perdre de temps et préparez-vous à découvrir les trésors cachés du Nord-Pas-de-Calais.</p>
                     <p>Chaque question vous rapproche un peu plus du premier maillot et du podium !</p>
                     <p><i><b>Bonne chance, et que la découverte de cette belle région commence !</b></i></p>
                 </div>
