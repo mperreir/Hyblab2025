5e2e438a272ac5d38116fc50c128468010796525
Théophile Tagne <tagnepierre9@gmail.com>
2025-01-28 17:57:01 +0100

    Add TransitionPage styles and structure: implement background, layout adjustments, and dynamic score display

--- lavoixdunord/frontend/src/TransitionPage/TransitionPage.js
+++ lavoixdunord/frontend/src/TransitionPage/TransitionPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import useBasename from '../hooks/useBasenameHook';
+import './TransitionPage.css';
 import ImageModal from '../components/ImageModal';
 
 const TransitionPage = () => {
@@ -14,95 +15,114 @@ const TransitionPage = () => {
     const finalScore = parseInt(localStorage.getItem("score")) || 0;
     // const difficulty = location.state?.difficulty || 1;
 
+    // Convertir le score en minutes et secondes
+    const minutes = Math.floor(finalScore / 60);
+    const seconds = finalScore % 60;
+
     let { level_id, difficulty } = useParams();
 
     const transition_data = [
         {
             id: 1,
             card_image: `${basename}images/Etape 2/map_etape2.png`,
-            maillot_image: `${basename}images/illustrations/MAILLOT_BLANC.png`,
-            paragraph1: "Félicitations, vous avez terminé la première étape de la 112e édition du Tour de France. Votre performance vous vaut le premier maillot, bravo !",
+            maillot_image_bg: `${basename}images/illustrations/bg_transition_blanc.png`,
+            maillot_image_icon: `${basename}images/illustrations/bg_transition_blanc_horloge.png`,
+            maillot_image: `${basename}images/illustrations/maillot_blanc.png`,
+            paragraph1: `Félicitations, vous avez terminé la première étape de la 112e édition du Tour de France en ${minutes} minute${minutes !== 1 ? 's' : ''} et ${seconds} seconde${seconds !== 1 ? 's' : ''}. Votre performance vous vaut le premier maillot, bravo !`,
             paragraph2: "Mais l’aventure ne s’arrête pas là !",
             paragraph3: "Place désormais à la deuxième étape qui vous mènera de Lauwin-Planque à Boulogne-sur-mer.",
             paragraph4: "Bonne chance pour décrocher votre deuxième maillot !",
             button_text: "ÉTAPE SUIVANTE",
-            navigate_to: `/start/${difficulty}/2`
+            navigate_to: `/start/${difficulty}/2`,
+            color: "#1A3347"
         },
         {
             id: 2,
             card_image: `${basename}images/Etape 3/map_etape3.png`,
+            maillot_image_bg: `${basename}images/illustrations/bg_transition_vert.png`,
+            maillot_image_icon: `${basename}images/illustrations/bg_transition_vert_horloge.png`,
             maillot_image: `${basename}images/illustrations/maillot_vert.png`,
-            paragraph1: "Félicitations, vous avez terminé la première étape de la 112e édition du Tour de France. Votre performance vous vaut le premier maillot, bravo !",
+            paragraph1: `Vous l’avez fait ! En franchissant la ligne d’arrivée de la deuxième étape, vous décrochez un deuxième maillot et poursuivez votre quête avec brio. Vous avez couru la deuxième étape en ${minutes} minute${minutes !== 1 ? 's' : ''} et ${seconds} seconde${seconds !== 1 ? 's' : ''}.`,
             paragraph2: "Mais l’aventure ne s’arrête pas là !",
-            paragraph3: "Place désormais à la troisième étape qui vous mènera de Lauwin-Planque à Boulogne-sur-mer.",
-            paragraph4: "Bonne chance pour décrocher votre troisième maillot !",
+            paragraph3: "Il reste un dernier défi à relever : le gain du troisième et dernier maillot.",
+            paragraph4: "À vous d’écrire la dernière page de votre épopée.",
             button_text: "ÉTAPE SUIVANTE",
-            navigate_to: `/start/${difficulty}/3`
+            navigate_to: `/start/${difficulty}/3`,
+            color: "#607F00"
         },
         {
             id: 3,
             card_image: null,
+            maillot_image_bg: `${basename}images/illustrations/bg_transition_jaune.png`,
             maillot_image: `${basename}images/illustrations/mallot_jaune.png`,
-            paragraph1: "Félicitations, vous avez franchi la ligne d’arrivée de cette dernière étape ! Après avoir traversé les paysages et les trésors du Nord-Pas-de-Calais, vous décrochez le troisième maillot et complétez votre collection. Votre parcours a été remarquable, et votre temps final, transformé en chrono fictif",
+            maillot_image_icon: `${basename}images/illustrations/bg_transition_jaune_horloge.png`,
+            paragraph1: `Félicitations, vous avez franchi la ligne d’arrivée de cette dernière étape ! Vous décrochez le troisième maillot. Votre parcours a été remarquable, et votre temps final, transformé en chrono fictif, est de ${minutes} minute${minutes !== 1 ? 's' : ''} et ${seconds} seconde${seconds !== 1 ? 's' : ''}.`,
             paragraph2: "Votre Tour de France s’achève ici !",
             paragraph3: "À Dunkerque, sur une note triomphale. Vous avez prouvé que l’Histoire et la culture de la région n’ont plus de secrets pour vous.",
             paragraph4: null,
             button_text: "TERMINER",
-            navigate_to: `/credit`
+            navigate_to: `/credit`,
+            color: "#ED8422"
         }
-    ]
+    ];
 
     const [currentTransition, setCurrentTransition] = React.useState(transition_data[parseInt(level_id) - 1]);
 
 
-    // Convertir le score en minutes et secondes
-    const minutes = Math.floor(finalScore / 60);
-    const seconds = finalScore % 60;
-
     return (
-        <div className="start_page_bg w-100 position-relative"
-            style={{ zIndex: 2 }}>
-            <div className="mt-84 text-center">
+        <div className="transition_bg"
+            style={{
+                backgroundImage: `url(${currentTransition.maillot_image_bg})`
+            }}
+            >
+            <div className="arbre"></div>
+            <div className="mt-76 text-center">
                 <h1 className="text-white">FÉLICITATIONS</h1>
+                <img src={currentTransition.maillot_image_icon}
+                    alt="maillot"
+                    className="maillot_icon"
+                    style={{
+                        height: '70px', objectFit: 'scale-down',
+                        marginTop: '40px',
+                        color: currentTransition.color
+                    }} />
 
-                {Boolean(currentTransition.card_image) && (<ImageModal
-                    imageSrc={currentTransition.card_image}
-                    altText={`Carte étape ${currentTransition.id}`}
-                    showModal={showModal}
-                    onClose={setShowModal}
-                />)}
+                <div className="p-5 pt-1 mt-2 fs-6 position-relative" style={{}}>
+                    <p style={{ color: currentTransition.color }}><b>{minutes} minute{minutes !== 1 ? "s" : ""} et {seconds} seconde{seconds !== 1 ? "s" : ""} !</b></p>
 
-                <div className="mx-3 p-3 fs-6 position-relative">
-                    <img
-                        src={currentTransition.maillot_image}
-                        className="position-absolute top-50 start-50 translate-middle opacity-25"
-                        style={{ objectFit: 'cover', zIndex: -1 }}
-                        alt="Maillot en arrière-plan"
-                    />
-
-                    <p>
+                    <p style={{}}>
                         {currentTransition.paragraph1}
                     </p>
 
-                    <p className=""><b>Score :</b> {minutes} minute{minutes !== 1 ? "s" : ""} et {seconds} seconde{seconds !== 1 ? "s" : ""}</p>
-    
-                    <p className="fw-bold">
+                    <img src={currentTransition.maillot_image}
+                        alt="maillot"
+                        className="maillot_icon"
+                        style={{
+                            height: '150px', objectFit: 'scale-down',
+                            color: currentTransition.color
+                        }} />
+
+                    <p className="fw-bold mt-2" style={{ color: currentTransition.color }}>
                         {currentTransition.paragraph2}
                     </p>
-                    <p>
+                    <p style={{}}>
                         {currentTransition.paragraph3}
                     </p>
-                    <p className="fw-bold">
+                    <p className="fw-bold" style={{ color: currentTransition.color }}>
                         {currentTransition.paragraph4}
                     </p>
+
+
+                    <button onClick={() => navigate(currentTransition.navigate_to)}
+                        className="btn btn-outline-light fw-bold mx-2"
+                        style={{
+                            borderColor: currentTransition.color,
+                            color: currentTransition.color
+                        }}
+                    >
+                        {currentTransition.button_text}
+                    </button>
                 </div>
-
-                <button onClick={() => navigate(currentTransition.navigate_to)}
-                    className="btn btn-outline-dark fw-bold mx-5"
-                >
-                    {currentTransition.button_text}
-                </button>
-
             </div>
         </div>
     );
