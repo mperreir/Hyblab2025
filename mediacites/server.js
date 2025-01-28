61aaf2d3e693eb98c2285956c97004e2fe7d61b1
Nolann Boyere <nolann.boyere@etu.univ-nantes.fr>
2025-01-28 16:59:24 +0100

    corrected server.js to change navigation link

--- mediacites/server.js
+++ mediacites/server.js
@@ -22,7 +22,7 @@ app.get(`/article`, function (req, res) {
 });
 
 app.get(`/navigation`, function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/navigation.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // This module is exported and served by the main server.js located
