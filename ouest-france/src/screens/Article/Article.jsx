2434ba252cd2f32b262cd95c7d94904d45485967
Flustix <victor.blot@gmail.com>
2025-01-30 01:28:06 +0100

    fix: revert article title

--- ouest-france/src/screens/Article/Article.jsx
+++ ouest-france/src/screens/Article/Article.jsx
@@ -40,31 +40,37 @@ const Article = ({data}) => {
         <section ref={sectionRef} className="relative article">
             {data.sections.map((section, index) => (
                 <section
-                    className={"h-screen w-full snap-start snap-always flex items-center justify-between px-32 py-12 relative " + ((index % 2 === 0) ? "flex-row" : "flex-row-reverse")}
+                    className={"h-screen w-full snap-start snap-always px-32 py-12 flex flex-col items-center justify-center"}
                     style={{fontFamily: 'Arial, sans-serif'}}
                     id={"article" + section.id}
                 >
-                    <div className="article-container w-1/3">
-                        <div className="triangle">
-                            <svg width="12" height="14" viewBox="0 0 15 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M13.5894 7.64486C14.2315 8.03413 14.2315 8.96587 13.5894 9.35514L1.5184 16.6728C0.851964 17.0768 -8.129e-07 16.597 -7.78834e-07 15.8176L-1.39105e-07 1.18235C-1.05039e-07 0.403021 0.851963 -0.0767918 1.5184 0.327214L13.5894 7.64486Z" fill="#CC2229"/>
-                            </svg>
+                    {index === 0 && (<h1 className="relative z-10 text-center max-w-3xl mb-8 text-black bg-white">{data.title}</h1>)}
+                    <div className={"w-full h-screen flex items-center justify-between " + ((index % 2 === 0) ? "flex-row" : "flex-row-reverse")}>
+                        <div className="article-container w-1/3">
+                            <div className="triangle">
+                                <svg width="12" height="14" viewBox="0 0 15 17" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M13.5894 7.64486C14.2315 8.03413 14.2315 8.96587 13.5894 9.35514L1.5184 16.6728C0.851964 17.0768 -8.129e-07 16.597 -7.78834e-07 15.8176L-1.39105e-07 1.18235C-1.05039e-07 0.403021 0.851963 -0.0767918 1.5184 0.327214L13.5894 7.64486Z"
+                                        fill="#CC2229"/>
+                                </svg>
+                            </div>
+                            <div className="article-content">
+                                <h2 className="title text-3xl text-black max-w-lg font-medium mb-4">{section.title}</h2>
+                                <p className="text_content text-black max-w-lg mb-6">{section.content}</p>
+                                <button className='text border border-black'
+                                        onClick={() => scrollToSection(`quiz${section.id}`)}>
+                                    Reprendre le quizz
+                                </button>
+                            </div>
                         </div>
-                        <div className="article-content">  
-                            <h2 className="title text-3xl text-black max-w-lg font-medium mb-4">{section.title}</h2>
-                            <p className="text_content text-black max-w-lg mb-6">{section.content}</p>
-                            <button className='text border border-black' onClick={() => scrollToSection(`quiz${section.id}`)}>
-                                Reprendre le quizz
-                            </button>
+                        <div className="w-1/3">
+                            <img
+                                src={section.image}
+                                alt={section.title}
+                                className="w-full h-auto shadow-lg"
+                            />
                         </div>
-                    </div>
-                    
-                    <div className="w-1/3">
-                        <img
-                            src={section.image}
-                            alt={section.title}
-                            className="w-full h-auto shadow-lg"
-                        />
                     </div>
                 </section>
             ))}
